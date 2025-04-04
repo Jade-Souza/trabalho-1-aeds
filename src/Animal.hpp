d541b7531365d6ede4d80b3e2112cbc73bee42af
Jade Giulia <jadegiulia3817@gmail.com>
2025-04-04 10:26:28 -0300

    class animal e simulador

---
+++ src/Animal.hpp
@@ -0,0 +1,17 @@
+#ifndef _SIMULADORF_HPP
+#define _SIMULADORF_HPP
+
+#include <iostream>
+#include <vector>
+#include <String>
+
+class Animal
+{
+    private:
+        int posicao_inicial, posicao_final;
+        
+    
+};
+
+
+#endif
---
+++ src/SimuladorF.hpp
@@ -0,0 +1,18 @@
+#ifndef _SIMULADORF_HPP
+#define _SIMULADORF_HPP
+#include "Animal.hpp"
+#include <iostream>
+#include <vector>
+
+class SimuladorF
+{
+    private:
+        int i,j;
+        std::vector<std::vector<int>> floresta;
+        Animal animal;
+
+
+};
+
+
+#endif
